64680e6b111162fd77bd61ddb77488bd8252511e
VenkataSaiSandeep <venkata.ssm@holosuit.com>
2023-06-05 15:03:33 +0530

    Plane detection

---
+++ Assets/Scripts/OnjectColourDetection.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class OnjectColourDetection : MonoBehaviour
+{
+   
+}
